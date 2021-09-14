9b0ca2bbf38bb59b2013d5a19c94a26cf927a537
HYivisir <843702140@qq.com>
2021-09-14 15:04:59 +0800

    feat: 添加分词函数和计算余弦相似度的函数

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import jieba
-from gensim import corpora
-from gensim.similarities import Similarity
+from gensim import corpora, models, similarities
 import re
 import sys
 import os
@@ -13,18 +12,78 @@ def remove_punctuation(string):
     res = jieba.lcut(string)
     return res
 
+# 将文章分句，
+def split_sentence(text):
+    head = '\u4e00'
+    tail = '\u9fa5'
+    word = ""
+    sentences = []  # 把拆分的句子保存下来
+    for each in range(len(text)):
+        if head <= text[each] <= tail:  # 中文编码范围
+            word += text[each]
+        elif text[each] == "，":  
+            # 如果是逗号，将句子push，然后将word清空
+            sentences.append(word)
+            word = ""
+        else:
+            continue
+    if word != '':
+        sentences.append(word)
+        word = ''
 
-def calc_similarity(origin, toCheck):
-    texts = [origin, toCheck]
-    dictionary = corpora.Dictionary(texts)
-    # 获取词语库
-    corpus = [dictionary.doc2bow(text) for text in texts]
-    # j计算余弦相似度
-    similarity = Similarity('-Similarity-index', corpus,
-                            num_features=len(dictionary))
-    test_corpus_1 = dictionary.doc2bow(origin)
-    cos_sim = similarity[test_corpus_1][1]
-    return cos_sim
+    return sentences
+
+def split_words(text):
+    # 利用jieba.luct进行分词，并将其保存在list列表中
+    list = [[word for word in jieba.lcut(sentence)] for sentence in text];
+    print(list)
+    return list
+
+def calc_Similiarity(origin_txt,to_check_txt):
+    sim_value = []
+    word_lenth = []
+    total_sum = 0
+    total_size = 0
+    ori_list = split_words(origin_txt)
+    ori_add_list = split_words(to_check_txt)
+    # 生成词典
+    dictionary = corpora.Dictionary(ori_list)
+    # 通过doc2bow稀疏向量生成语料库
+    corpus = [dictionary.doc2bow(word) for word in ori_list]
+    # 通过TF模型算法，计算出tf值
+    tf = models.TfidfModel(corpus)
+    # 通过token2id得到特征数（字典里面键的个数）
+    num_features = len(dictionary.token2id.keys())
+    # 计算稀疏矩阵相似度，并建立索引
+    index = similarities.MatrixSimilarity(tf[corpus], num_features=num_features)
+    # 每句长度-单个变量
+    word_size = 0
+    size = 0
+    sim = 0
+    for word in range(len(ori_add_list)):
+        # 新的稀疏向量
+        new_vector = dictionary.doc2bow(ori_add_list[word])
+        # 算出文章的相似度
+        sim_list = index[tf[new_vector]]
+        # 选出最大相似度
+        sim = max(sim_list)
+        # 加入相似度列表
+        sim_value.append(sim)
+        # 相似文章每句长度值
+        word_size = len(ori_add_list[word])
+        # 文章总长度值
+        size += word_size
+        # 加入长度列表
+        word_lenth.append(word_size)
+    total_size = size
+    for i in range(len(word_lenth)):
+        total_sum += word_lenth[i] * sim_value[i]
+
+    # 加权求平均
+    ans = total_sum / total_size
+
+    return ans
+
 
 
 def get_contents(path):
